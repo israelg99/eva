d7d6ebaea91e81bc0481e9961a30e8e08c75f904
Israel Gilyadov <israelg99@gmail.com>
2017-01-23 10:52:10 -0600

    Update utility imports for infer

--- eva/examples/infer.py
+++ eva/examples/infer.py
@@ -5,12 +5,12 @@ import keras
 
 from eva.layers.masked_convolution2d import MaskedConvolution2D
 
-from eva.util.mutil import Mutil
-from eva.util.nutil import Nutil
+from eva.util.mutil import infer
+from eva.util.nutil import to_rgb
 
 count = 1
 model = keras.models.load_model('eva/examples/pixelcnn.h5', custom_objects={'MaskedConvolution2D':MaskedConvolution2D})
 
-for image in [Nutil.to_rgb(Mutil.infer(model)) for _ in range(count)]:
+for image in [to_rgb(infer(model)) for _ in range(count)]:
     plt.imshow(image, interpolation='nearest')
     plt.show()
