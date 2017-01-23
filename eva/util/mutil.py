9688ed552ac5d792c983b7fe36df0a2889f77094
Israel Gilyadov <israelg99@gmail.com>
2017-01-23 10:51:17 -0600

    Add binarize; pythonize utility funcs

--- eva/util/mutil.py
+++ eva/util/mutil.py
@@ -1,14 +1,12 @@
 import numpy as np
 
 
-class Mutil(object):
-    @staticmethod
-    def infer(model):
-        pixels = np.zeros(model.input_shape[1:])
-        rows, cols, channels = pixels.shape
-        for row in range(rows):
-            for pixel in range(cols):
-                for channel in range(channels):
-                    pixels[row, pixel, channel] = model.predict_on_batch(pixels[np.newaxis])[0][row, pixel, channel]
+def infer(model):
+    pixels = np.zeros(model.input_shape[1:])
+    rows, cols, channels = pixels.shape
+    for row in range(rows):
+        for pixel in range(cols):
+            for channel in range(channels):
+                pixels[row, pixel, channel] = model.predict_on_batch(pixels[np.newaxis])[0][row, pixel, channel]
 
-        return pixels
+    return pixels
--- eva/util/nutil.py
+++ eva/util/nutil.py
@@ -1,7 +1,12 @@
 import numpy as np
 
 
-class Nutil(object):
-    @staticmethod
-    def to_rgb(pixels):
-        return np.repeat(pixels, 3 if pixels.shape[2] == 1 else 1, 2)
+def to_rgb(pixels):
+    return np.repeat(pixels, 3 if pixels.shape[2] == 1 else 1, 2)
+
+def binarize(arr, generate=np.random.uniform):
+    """
+    Stochastically binarize values in [0, 1] by treating them as p-values of
+    a Bernoulli distribution.
+    """
+    return (generate(size=arr.shape) < arr).astype('float32')
