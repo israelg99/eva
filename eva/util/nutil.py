73b61983de6ff655b4f11205c0acd2b2f92915f4
Israel Gilyadov <israelg99@gmail.com>
2017-01-25 18:51:34 -0600

    Remove comment; change to int

--- eva/util/nutil.py
+++ eva/util/nutil.py
@@ -5,8 +5,4 @@ def to_rgb(pixels):
     return np.repeat(pixels, 3 if pixels.shape[2] == 1 else 1, 2)
 
 def binarize(arr, generate=np.random.uniform):
-    """
-    Stochastically binarize values in [0, 1] by treating them as p-values of
-    a Bernoulli distribution.
-    """
-    return (generate(size=arr.shape) < arr).astype('float32')
+    return (generate(size=arr.shape) < arr).astype('i')
