28c917e014d2f52e7c2f545c14fe665d4715b853
Israel Gilyadov <israelg99@gmail.com>
2017-01-31 23:57:47 -0600

    Remove jupyter nonsense

--- eva/examples/infer.py
+++ eva/examples/infer.py
@@ -9,11 +9,8 @@ from eva.layers.color_extract import ColorExtract
 from eva.util.mutil import infer
 from eva.util.nutil import to_rgb
 
-count = 1
 model = keras.models.load_model('eva/examples/pixelcnn.h5', custom_objects={'MaskedConvolution2D':MaskedConvolution2D, 'ColorExtract':ColorExtract})
 
-model.summary()
-image[0]
 for image in [infer(model) for _ in range(count)]:
     plt.imshow(image, interpolation='nearest')
     plt.show()
