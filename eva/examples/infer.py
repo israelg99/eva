e268aa0dc6d6e2631acc3836ffa1ae23e0efe3f1
Israel Gilyadov <israelg99@gmail.com>
2017-01-31 23:54:00 -0600

    Update inference for RGB

--- eva/examples/infer.py
+++ eva/examples/infer.py
@@ -4,13 +4,16 @@ from matplotlib import pyplot as plt
 import keras
 
 from eva.layers.masked_convolution2d import MaskedConvolution2D
+from eva.layers.color_extract import ColorExtract
 
 from eva.util.mutil import infer
 from eva.util.nutil import to_rgb
 
-count = 10
-model = keras.models.load_model('eva/examples/pixelcnn.h5', custom_objects={'MaskedConvolution2D':MaskedConvolution2D})
+count = 1
+model = keras.models.load_model('eva/examples/pixelcnn.h5', custom_objects={'MaskedConvolution2D':MaskedConvolution2D, 'ColorExtract':ColorExtract})
 
-for image in [to_rgb(infer(model)) for _ in range(count)]:
+model.summary()
+image[0]
+for image in [infer(model) for _ in range(count)]:
     plt.imshow(image, interpolation='nearest')
     plt.show()
--- eva/util/mutil.py
+++ eva/util/mutil.py
@@ -3,11 +3,11 @@ import numpy as np
 from eva.util.nutil import binarize
 
 def infer(model):
-    pixels = np.zeros(model.input_shape[1:])
+    pixels = np.random.rand(*model.input_shape[1:])
     rows, cols, channels = pixels.shape
     for row in range(rows):
-        for pixel in range(cols):
+        for col in range(cols):
             for channel in range(channels):
-                pixels[row, pixel, channel] = binarize(model.predict_on_batch(pixels[np.newaxis])[0])[row, pixel, channel]
+                pixels[row, col, channel] = np.argmax(model.predict_on_batch(pixels[np.newaxis])[channel][0][row*cols+col])
 
     return pixels
