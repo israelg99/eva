95173c2a25dc3b10814ebd0a0dfbc549ea4702fc
Israel Gilyadov <israelg99@gmail.com>
2017-02-06 16:20:08 +0200

    Add infer jupyter debugging info

--- eva/examples/infer.py
+++ eva/examples/infer.py
@@ -6,10 +6,30 @@ import keras
 from eva.layers.masked_convolution2d import MaskedConvolution2D
 from eva.layers.color_extract import ColorExtract
 
-from eva.util.mutil import infer
-from eva.util.nutil import to_rgb
+from eva.util.mutil import infer, generate
 
-model = keras.models.load_model('eva/examples/pixelcnn.h5', custom_objects={'MaskedConvolution2D':MaskedConvolution2D, 'ColorExtract':ColorExtract})
+count = 1
+
+model = keras.models.load_model('eva/examples/model.h5', custom_objects={'MaskedConvolution2D':MaskedConvolution2D})
+
+model.get_layer(index=-7).get_weights()[0].shape
+
+#%%
+import numpy as np #RRR
+pixels = np.zeros(shape=model.input_shape[1:])
+p = model.predict_on_batch(pixels[np.newaxis])
+t = np.zeros(256)
+
+for c in p:
+    for i in c[0]:
+        t[np.argmax(i)] += 1
+
+print(t)
+
+#%%
+for image in [generate(model) for _ in range(count)]:
+    plt.imshow(image, interpolation='nearest')
+    plt.show()
 
 for image in [infer(model) for _ in range(count)]:
     plt.imshow(image, interpolation='nearest')
