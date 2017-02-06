9f299e96da2cede6482893970fbcdf91db8d28a1
Israel Gilyadov <israelg99@gmail.com>
2017-02-06 16:21:27 +0200

    Add debug infer/generation utils

--- eva/util/mutil.py
+++ eva/util/mutil.py
@@ -2,8 +2,8 @@ import numpy as np
 
 from eva.util.nutil import binarize
 
-def infer(model):
-    pixels = np.random.rand(*model.input_shape[1:])
+def generate(model):
+    pixels = np.zeros(shape=model.input_shape[1:])
     rows, cols, channels = pixels.shape
     for row in range(rows):
         for col in range(cols):
@@ -11,3 +11,18 @@ def infer(model):
                 pixels[row, col, channel] = np.argmax(model.predict_on_batch(pixels[np.newaxis])[channel][0][row*cols+col])
 
     return pixels
+
+def infer(model):
+    # TODO: Improve generation.
+    pixels = np.zeros(shape=model.input_shape[1:])
+    pixels[0,0,0] = np.random.randint(256)
+    rows, cols, channels = pixels.shape
+    for row in range(rows):
+        for col in range(cols):
+            for channel in range(channels):
+                if row == 0 and col == 0 and channel == 0:
+                    continue
+                pixels[row, col, channel] = np.argmax(model.predict_on_batch(pixels[np.newaxis])[channel][0][row*cols+col])
+
+    return pixels
+
