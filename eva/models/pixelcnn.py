380b9cddd20c1526e11dc4301ef11abd94b1db11
Israel Gilyadov <israelg99@gmail.com>
2017-02-08 11:25:33 +0200

    Return to 3 RGB output with Keras softmax

--- eva/models/pixelcnn.py
+++ eva/models/pixelcnn.py
@@ -34,32 +34,32 @@ def PixelCNN(input_shape, filters, blocks, build=True):
     # model = Reshape((input_shape[0] * input_shape[1], 256))(model)
     # model = FuckingSoftmax()(model)
 
-    model = MaskedConvolution2D(256, 1, 1)(model)
-    model = Reshape((input_shape[0] * input_shape[1], 256))(model)
-    model = Activation('softmax')(model)
+    # model = MaskedConvolution2D(256, 1, 1)(model)
+    # model = Reshape((input_shape[0] * input_shape[1], 256))(model)
+    # model = Activation('softmax')(model)
 
-    # red = MaskedConvolution2D(256, 1, 1)(model)
-    # red = Reshape((input_shape[0] * input_shape[1], 256))(red)
-    # red = Activation('softmax', name='red')(red)
+    red = MaskedConvolution2D(256, 1, 1)(model)
+    red = Reshape((input_shape[0] * input_shape[1], 256))(red)
+    red = Activation('softmax', name='red')(red)
 
-    # green = MaskedConvolution2D(256, 1, 1)(model)
-    # green = Reshape((input_shape[0] * input_shape[1], 256))(green)
-    # green = Activation('softmax', name='green')(green)
+    green = MaskedConvolution2D(256, 1, 1)(model)
+    green = Reshape((input_shape[0] * input_shape[1], 256))(green)
+    green = Activation('softmax', name='green')(green)
 
-    # blue = MaskedConvolution2D(256, 1, 1)(model)
-    # blue = Reshape((input_shape[0] * input_shape[1], 256))(blue)
-    # blue = Activation('softmax', name='blue')(blue)
+    blue = MaskedConvolution2D(256, 1, 1)(model)
+    blue = Reshape((input_shape[0] * input_shape[1], 256))(blue)
+    blue = Activation('softmax', name='blue')(blue)
 
     # TODO: Make is scalable to any amount of channels.
 
     if build:
-        # model = Model(input=input_map, output=[red, green, blue])
-        # model.compile(optimizer=Nadam(clipnorm=1., clipvalue=1.),
-        #               loss={    'red':   'sparse_categorical_crossentropy',
-        #                         'green': 'sparse_categorical_crossentropy',
-        #                         'blue':  'sparse_categorical_crossentropy'})
-        model = Model(input=input_map, output=model)
-        model.compile(optimizer=Nadam(),
-                      loss='sparse_categorical_crossentropy')
+        model = Model(input=input_map, output=[red, green, blue])
+        model.compile(optimizer=Nadam(clipnorm=1., clipvalue=1.),
+                      loss={    'red':   'sparse_categorical_crossentropy',
+                                'green': 'sparse_categorical_crossentropy',
+                                'blue':  'sparse_categorical_crossentropy'})
+        # model = Model(input=input_map, output=model)
+        # model.compile(optimizer=Nadam(),
+        #               loss='sparse_categorical_crossentropy')
 
     return model
