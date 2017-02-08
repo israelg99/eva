8e85b465d1bfaf86d1708431e548d19b0888d6d9
Israel Gilyadov <israelg99@gmail.com>
2017-02-08 09:56:40 +0200

    Change to TF softmax

--- eva/layers/fuckingsoftmax.py
+++ eva/layers/fuckingsoftmax.py
@@ -1,12 +1,13 @@
 from keras.engine import Layer
-from keras import backend as K
+
+import tensorflow as tf
 
 class FuckingSoftmax(Layer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def call(self, x, mask=None):
-        return K.softmax(x)
+        return tf.nn.softmax(x)
 
     def get_config(self):
         return dict(list(super().get_config().items()) + list({'fucking': True}.items()))
