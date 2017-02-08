98c1111ee26de1df8f240afbfa543c8985e32ea8
Israel Gilyadov <israelg99@gmail.com>
2017-02-08 08:03:07 +0200

    Add generate func

--- eva/util/mutil.py
+++ eva/util/mutil.py
@@ -1,28 +1,12 @@
 import numpy as np
 
-from eva.util.nutil import binarize
-
 def generate(model):
     pixels = np.zeros(shape=model.input_shape[1:])
     rows, cols, channels = pixels.shape
     for row in range(rows):
         for col in range(cols):
             for channel in range(channels):
-                pixels[row, col, channel] = np.argmax(model.predict_on_batch(pixels[np.newaxis])[channel][0][row*cols+col])
+                pixels[row, col, channel] = np.random.choice(256, p=model.predict_on_batch(pixels[np.newaxis])[channel][row*cols+col])
+                # pixels[row, col, channel] = np.random.choice(256, p=model.predict_on_batch(pixels[np.newaxis])[channel][0][row*cols+col])
 
     return pixels
-
-def infer(model):
-    # TODO: Improve generation.
-    pixels = np.zeros(shape=model.input_shape[1:])
-    pixels[0,0,0] = np.random.randint(256)
-    rows, cols, channels = pixels.shape
-    for row in range(rows):
-        for col in range(cols):
-            for channel in range(channels):
-                if row == 0 and col == 0 and channel == 0:
-                    continue
-                pixels[row, col, channel] = np.argmax(model.predict_on_batch(pixels[np.newaxis])[channel][0][row*cols+col])
-
-    return pixels
-
