4bafe1032ebd0c6c538dc49bc5aec2d9d0b9c244
Israel Gilyadov <israelg99@gmail.com>
2017-02-08 13:17:36 +0200

    Update image generation

--- eva/util/mutil.py
+++ eva/util/mutil.py
@@ -6,7 +6,6 @@ def generate(model):
     for row in range(rows):
         for col in range(cols):
             for channel in range(channels):
-                pixels[row, col, channel] = np.random.choice(256, p=model.predict_on_batch(pixels[np.newaxis])[channel][row*cols+col])
-                # pixels[row, col, channel] = np.random.choice(256, p=model.predict_on_batch(pixels[np.newaxis])[channel][0][row*cols+col])
+                pixels[row, col, channel] = np.random.choice(256, p=model.predict_on_batch(pixels[np.newaxis])[channel][0][row*cols+col])
 
     return pixels
