78840fb124e6e49acfa0f73ea73eddfa8558f598
Israel Gilyadov <israelg99@gmail.com>
2017-02-08 08:03:31 +0200

    Improve infer script

--- eva/examples/infer.py
+++ eva/examples/infer.py
@@ -4,33 +4,21 @@ from matplotlib import pyplot as plt
 import keras
 
 from eva.layers.masked_convolution2d import MaskedConvolution2D
-from eva.layers.color_extract import ColorExtract
 
-from eva.util.mutil import infer, generate
+from eva.util.mutil import generate
 
-count = 1
+count = 10
 
 model = keras.models.load_model('eva/examples/model.h5', custom_objects={'MaskedConvolution2D':MaskedConvolution2D})
 
-model.get_layer(index=-7).get_weights()[0].shape
-
-#%%
-import numpy as np #RRR
 pixels = np.zeros(shape=model.input_shape[1:])
-p = model.predict_on_batch(pixels[np.newaxis])
-t = np.zeros(256)
-
-for c in p:
-    for i in c[0]:
-        t[np.argmax(i)] += 1
-
-print(t)
+rows, cols, channels = pixels.shape
+for row in range(rows):
+    for col in range(cols):
+        pixels[row, col, 0] = np.random.choice(256, p=model.predict_on_batch(pixels[np.newaxis])[0][row*cols+col])
+pixels = pixels[:, :, 0]
 
 #%%
 for image in [generate(model) for _ in range(count)]:
-    plt.imshow(image, interpolation='nearest')
-    plt.show()
-
-for image in [infer(model) for _ in range(count)]:
     plt.imshow(image, interpolation='nearest')
     plt.show()
