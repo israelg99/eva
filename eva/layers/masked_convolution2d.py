26112b9598bd27d717838d9d44d67ab6ea163753
Israel Gilyadov <israelg99@gmail.com>
2017-02-08 13:17:19 +0200

    Add mono functionality in masks

--- eva/layers/masked_convolution2d.py
+++ eva/layers/masked_convolution2d.py
@@ -6,10 +6,11 @@ from keras import backend as K
 from keras.layers import Convolution2D
 
 class MaskedConvolution2D(Convolution2D):
-    def __init__(self, *args, mask='B' , n_channels=3, **kwargs):
+    def __init__(self, *args, mask='B' , n_channels=3, mono=True, **kwargs):
         super().__init__(*args, **kwargs)
         self.mask_type = mask
         self.n_channels = n_channels
+        self.mono = mono
 
         # TODO: Define mask here? we can use filter_size and number of filters to predict the weights shape, however, build() is still a safer place for it.
         self.mask = None
@@ -25,15 +26,15 @@ class MaskedConvolution2D(Convolution2D):
         self.mask[math.ceil(filter_center):] = 0
         self.mask[math.floor(filter_center):, math.ceil(filter_center):] = 0
 
-        op = np.greater_equal if self.mask_type == 'A' else np.greater
-
-        for i in range(self.n_channels):
-            for j in range(self.n_channels):
-                if op(i, j):
-                    self.mask[math.floor(filter_center), math.floor(filter_center), i::self.n_channels, j::self.n_channels] = 0
-
-        # if self.mask_type == 'A':
-        #     self.mask[math.floor(filter_center), math.floor(filter_center)] = 0
+        if self.mono:
+            if self.mask_type == 'A':
+                self.mask[math.floor(filter_center), math.floor(filter_center)] = 0
+        else:
+            op = np.greater_equal if self.mask_type == 'A' else np.greater
+            for i in range(self.n_channels):
+                for j in range(self.n_channels):
+                    if op(i, j):
+                        self.mask[math.floor(filter_center), math.floor(filter_center), i::self.n_channels, j::self.n_channels] = 0
 
         self.mask = K.variable(self.mask)
 
@@ -54,4 +55,4 @@ class MaskedConvolution2D(Convolution2D):
         return output
 
     def get_config(self):
-        return dict(list(super().get_config().items()) + list({'mask': self.mask_type}.items()))
+        return dict(list(super().get_config().items()) + list({'mask': self.mask_type, 'n_channels': self.n_channels, 'mono': self.mono}.items()))
