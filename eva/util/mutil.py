07ed2c22cc2daed3ad68e7f95042bcb4c33e8f02
Israel Gilyadov <israelg99@gmail.com>
2017-02-13 18:39:43 +0200

    Improve generation

--- eva/util/mutil.py
+++ eva/util/mutil.py
@@ -1,11 +1,17 @@
 import numpy as np
 
-def generate(model):
+def generate(model, deterministic=False):
+    get_color = lambda model, pixels, row, col, channel: model.predict_on_batch(pixels[np.newaxis])[channel][0][row*cols+col]
+    to_255 = lambda color: color/255
+
     pixels = np.zeros(shape=model.input_shape[1:])
     rows, cols, channels = pixels.shape
     for row in range(rows):
         for col in range(cols):
             for channel in range(channels):
-                pixels[row, col, channel] = np.random.choice(256, p=model.predict_on_batch(pixels[np.newaxis])[channel][0][row*cols+col])
+                if deterministic:
+                    pixels[row, col, channel] = to_255(np.argmax(get_color(model, pixels, row, col, channel)))
+                    continue
+                pixels[row, col, channel] = to_255(np.random.choice(256, p=get_color(model, pixels, row, col, channel)))
 
     return pixels
