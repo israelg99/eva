14f4fe95be0501142f929a9b7bec807fd14e3d6f
Israel Gilyadov <israelg99@gmail.com>
2017-02-16 12:04:05 +0200

    Fix residual blocks to be on-par with paper.

--- eva/layers/residual_block.py
+++ eva/layers/residual_block.py
@@ -1,22 +1,22 @@
 from keras.layers import Convolution2D, Merge
 from keras.layers.advanced_activations import PReLU
-from keras.engine.topology import merge
 
 from eva.layers.masked_convolution2d import MaskedConvolution2D
 
 def ResidualBlock(model, filters):
     # 2h -> h
-    block = Convolution2D(filters//2, 1, 1)(model)
-    block = PReLU()(block)
+    block = PReLU()(model)
+    block = MaskedConvolution2D(filters//2, 1, 1)(block)
 
     # h 3x3 -> h
-    block = MaskedConvolution2D(filters//2, 3, 3, border_mode='same')(block)
     block = PReLU()(block)
+    block = MaskedConvolution2D(filters//2, 3, 3, border_mode='same')(block)
 
     # h -> 2h
-    block = Convolution2D(filters, 1, 1)(block)
+    block = PReLU()(block)
+    block = MaskedConvolution2D(filters, 1, 1)(block)
 
-    return PReLU()(merge([model, block], mode='sum'))
+    return Merge(mode='sum')([model, block])
 
 def ResidualBlockList(model, filters, length):
     for _ in range(length):
