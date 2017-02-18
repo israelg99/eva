86029ff0bad7fbaeece877b5132f5a26c25383b2
Israel Gilyadov <israelg99@gmail.com>
2017-02-18 20:30:47 +0200

    Clean and improve CIFAR-10

--- eva/examples/cifar10.py
+++ eva/examples/cifar10.py
@@ -1,4 +1,4 @@
-#%% Imports.
+#%% Setup.
 import numpy as np
 
 import keras
@@ -17,12 +17,6 @@ from keras.callbacks import TensorBoard, ModelCheckpoint
 from eva.models.pixelcnn import PixelCNN
 from eva.models.gated_pixelcnn import GatedPixelCNN
 
-#%% Arguments.
-batch_size = 32
-nb_epoch = 200
-
-data_augmentation = True
-
 #%% Data.
 (train, _), (test, _) = cifar10.load_data()
 data = np.concatenate((train, test), axis=0)
@@ -30,16 +24,13 @@ data = data.astype('float32')
 data /= 255
 
 #%% Model.
-# model = PixelCNN(data.shape[1:], 128//data.shape[-1]*data.shape[-1], 12)
-model = GatedPixelCNN(data.shape[1:], 128//data.shape[-1]*data.shape[-1], 12)
+# model = PixelCNN(data.shape[1:], 126, 1)
+model = GatedPixelCNN(data.shape[1:], 126, 1)
 
 #%% Train.
-# model.fit({'input_map': data},
-#           {'red': (np.expand_dims(data[:, :, :, 0].reshape(data.shape[0], data.shape[1]*data.shape[2]), -1)*255).astype(int),
-#            'green': (np.expand_dims(data[:, :, :, 1].reshape(data.shape[0], data.shape[1]*data.shape[2]), -1)*255).astype(int),
-#            'blue': (np.expand_dims(data[:, :, :, 2].reshape(data.shape[0], data.shape[1]*data.shape[2]), -1)*255).astype(int)},
-#           batch_size=batch_size, nb_epoch=nb_epoch,
-#           verbose=1, callbacks=[TensorBoard(), ModelCheckpoint('model.h5')])
-model.fit(data, (np.expand_dims(data.reshape(data.shape[0], data.shape[1]*data.shape[2], data.shape[3]), -1)*255).astype(int))
+model.fit(data,
+          [(np.expand_dims(data[:, :, :, 0].reshape(data.shape[0], data.shape[1]*data.shape[2]), -1)*255).astype(int),
+           (np.expand_dims(data[:, :, :, 1].reshape(data.shape[0], data.shape[1]*data.shape[2]), -1)*255).astype(int),
+           (np.expand_dims(data[:, :, :, 2].reshape(data.shape[0], data.shape[1]*data.shape[2]), -1)*255).astype(int)],
           batch_size=batch_size, nb_epoch=nb_epoch,
-          verbose=1, callbacks=[TensorBoard(), ModelCheckpoint('model.h5', save_weights_only=True)])
+          verbose=1, callbacks=[TensorBoard(), ModelCheckpoint('model.h5', save_weights_only=True)]) # Only weights because Keras is a bitch.
