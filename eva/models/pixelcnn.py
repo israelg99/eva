58bab9291c85edc3f13d3dc0659eff3c17201eb1
Israel Gilyadov <israelg99@gmail.com>
2017-02-18 20:12:59 +0200

    Improve pixelcnn namings and comments

--- eva/models/pixelcnn.py
+++ eva/models/pixelcnn.py
@@ -11,34 +11,32 @@ from eva.layers.masked_convolution2d import MaskedConvolution2D
 def PixelCNN(input_shape, filters, blocks, build=True):
     width, height, channels = input_shape
 
+    # TODO: Make it scalable to any amount of channels.
     input_map = Input(shape=input_shape, name='input_map')
 
-    model = MaskedConvolution2D(filters, 7, 7, mask='A', border_mode='same')(input_map)
+    model = MaskedConvolution2D(filters, 7, 7, mask='A', border_mode='same', name='masked2d_A')(input_map)
 
-    model = ResidualBlockList(model, filters, blocks)
+    model = ResidualBlockList(filters, blocks)(model)
     model = PReLU()(model)
 
     model = MaskedConvolution2D(filters, 1, 1)(model)
     model = PReLU()(model)
 
-    model = MaskedConvolution2D(3*256, 1, 1)(model)
+    model = MaskedConvolution2D(3*256, 1, 1, name='channels_mult_palette')(model)
+    model = Reshape((input_shape[0], input_shape[1], 256, input_shape[2]), name='palette_channels')(model)
 
     # TODO: Make it scalable to any amount of channels.
 
-    model = Reshape((input_shape[0], input_shape[1], 256, input_shape[2]))(model)
-
-    # TODO: Make it scalable to any amount of channels.
-
-    red = Lambda(lambda x: x[:, :, :, :, 0])(model)
-    red = Reshape((input_shape[0] * input_shape[1], 256))(red)
+    red = Lambda(lambda x: x[:, :, :, :, 0], name='red_extract')(model)
+    red = Reshape((input_shape[0] * input_shape[1], 256), name='hw_red-palette')(red)
     red = Activation('softmax', name='red')(red)
 
-    green = Lambda(lambda x: x[:, :, :, :, 1])(model)
-    green = Reshape((input_shape[0] * input_shape[1], 256))(green)
+    green = Lambda(lambda x: x[:, :, :, :, 1], name='green_extract')(model)
+    green = Reshape((input_shape[0] * input_shape[1], 256), name='hw_green-palette')(green)
     green = Activation('softmax', name='green')(green)
 
-    blue = Lambda(lambda x: x[:, :, :, :, 2])(model)
-    blue = Reshape((input_shape[0] * input_shape[1], 256))(blue)
+    blue = Lambda(lambda x: x[:, :, :, :, 2], name='blue_extract')(model)
+    blue = Reshape((input_shape[0] * input_shape[1], 256), name='hw_blue-palette')(blue)
     blue = Activation('softmax', name='blue')(blue)
 
     # TODO: Make is scalable to any amount of channels.
