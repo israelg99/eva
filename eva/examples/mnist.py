b21dc786c07405b2a498c227b0a5b2c63669c033
Israel Gilyadov <israelg99@gmail.com>
2017-02-19 12:21:57 +0200

    Add more epochs MNIST

--- eva/examples/mnist.py
+++ eva/examples/mnist.py
@@ -38,5 +38,5 @@ model.fit(data,
           [(np.expand_dims(data[:, :, :, 0].reshape(data.shape[0], data.shape[1]*data.shape[2]), -1)*255).astype(int),
            (np.expand_dims(data[:, :, :, 1].reshape(data.shape[0], data.shape[1]*data.shape[2]), -1)*255).astype(int),
            (np.expand_dims(data[:, :, :, 2].reshape(data.shape[0], data.shape[1]*data.shape[2]), -1)*255).astype(int)],
-          batch_size=32, nb_epoch=40,
+          batch_size=32, nb_epoch=200,
           verbose=1, callbacks=[TensorBoard(), ModelCheckpoint('model.h5', save_weights_only=True)]) # Only weights because Keras is a bitch.
