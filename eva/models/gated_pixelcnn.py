dc5ac4de2d40ceaadc594a9eb398b5356e6df261
Israel Gilyadov <israelg99@gmail.com>
2017-02-19 13:26:38 +0200

    Implement OutChannels and more tweaks

--- eva/models/gated_pixelcnn.py
+++ eva/models/gated_pixelcnn.py
@@ -1,19 +1,17 @@
 from keras.models import Model
-from keras.layers import Input, Convolution2D, Activation, Flatten, Dense, Reshape, Lambda
+from keras.layers import Input, Convolution2D
 from keras.layers.advanced_activations import PReLU
-from keras.engine.topology import merge
 from keras.optimizers import Nadam
-import keras.backend.tensorflow_backend as K
 
 from eva.layers.gated_cnn import GatedCNN, GatedCNNs
+from eva.layers.out_channels import OutChannels
 from eva.layers.masked_convolution2d import MaskedConvolution2D
 
 def GatedPixelCNN(input_shape, filters, blocks, latent=None, build=True):
     height, width, channels = input_shape
-    palette = 256
+    palette = 256 # TODO: Make it scalable to any amount of palette.
 
-    # TODO: Make it scalable to any amount of palettes.
-    input_img = Input(shape=input_shape, name=str(channels)+'_channels_256_palette')
+    input_img = Input(shape=input_shape, name=str(channels)+'_channels_'+str(palette)+'_palette')
 
     latent_vector = None
     if latent is not None:
@@ -25,17 +23,7 @@ def GatedPixelCNN(input_shape, filters, blocks, latent=None, build=True):
         model = Convolution2D(filters, 1, 1, border_mode='valid')(model)
         model = PReLU()(model)
 
-    if channels == 1:
-        outs = Convolution2D(1, 1, 1, activation='sigmoid', border_mode='valid')(model)
-    else:
-        out = Convolution2D(palette*channels, 1, 1, border_mode='valid', name='channels_mult_palette')(model)
-        out = Reshape((height, width, palette, channels), name='palette_channels')(out)
-
-        outs = [None] * channels
-        for i in range(channels):
-            outs[i] = Lambda(lambda x: x[:, :, :, :, i], name='channel'+str(i)+'_extract')(out)
-            outs[i] = Reshape((height * width, palette), name='hw_palette'+str(i))(outs[i])
-            outs[i] = Activation('softmax', name='channel'+str(i))(outs[i])
+    outs = OutChannels(*input_shape, masked=False, palette=palette)(model)
 
     if build:
         model = Model(input=[input_img, latent_vector] if latent is not None else input_img, output=outs)
--- eva/models/pixelcnn.py
+++ eva/models/pixelcnn.py
@@ -1,38 +1,28 @@
 from keras.models import Model
-from keras.layers import Input, Convolution2D, Activation, Flatten, Dense, Reshape, Lambda
+from keras.layers import Input
 from keras.layers.advanced_activations import PReLU
-from keras.engine.topology import merge
 from keras.optimizers import Nadam
-import keras.backend.tensorflow_backend as K
 
 from eva.layers.residual_block import ResidualBlockList
+from eva.layers.out_channels import OutChannels
 from eva.layers.masked_convolution2d import MaskedConvolution2D
 
 def PixelCNN(input_shape, filters, blocks, build=True):
     height, width, channels = input_shape
+    palette = 256 # TODO: Make it scalable to any amount of palette.
 
-    input_img = Input(shape=input_shape, name='input_img')
+    input_img = Input(shape=input_shape, name=str(channels)+'_channels_'+str(palette)+'_palette')
 
     model = MaskedConvolution2D(filters, 7, 7, mask='A', border_mode='same', name='masked2d_A')(input_img)
 
     model = ResidualBlockList(filters, blocks)(model)
     model = PReLU()(model)
 
-    model = MaskedConvolution2D(filters, 1, 1)(model)
-    model = PReLU()(model)
+    for _ in range(2):
+        model = MaskedConvolution2D(filters, 1, 1, border_mode='valid')(model)
+        model = PReLU()(model)
 
-    # TODO: Make it scalable to any amount of palette.
-    if channels == 1:
-        outs = Convolution2D(1, 1, 1, activation='sigmoid', border_mode='valid')(model)
-    else:
-        out = MaskedConvolution2D(channels*256, 1, 1, name='channels_mult_palette')(model)
-        out = Reshape((height, width, 256, channels), name='palette_channels')(out)
-
-        outs = [None] * channels
-        for i in range(channels):
-            outs[i] = Lambda(lambda x: x[:, :, :, :, 0], name='channel'+str(i)+'_extract')(out)
-            outs[i] = Reshape((height * width, 256), name='hw_palette'+str(i))(outs[i])
-            outs[i] = Activation('softmax', name='channel'+str(i))(outs[i])
+    outs = OutChannels(*input_shape, masked=True, palette=palette)(model)
 
     if build:
         model = Model(input=input_img, output=outs)
