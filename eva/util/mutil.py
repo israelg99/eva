080d5157a22fd9a73763887cf2d03af32a5f1d44
Israel Gilyadov <israelg99@gmail.com>
2017-02-21 23:18:08 +0200

    Engineer and improve utility funcs; sparse_labels

--- eva/util/mutil.py
+++ eva/util/mutil.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from tqdm import tqdm
+
 def generate(model, latent=None, batch=1, deterministic=False):
     get_color = lambda model, pixels, cols, row, col, channel, latent=None: model.predict_on_batch(pixels if latent is None else [pixels, latent])[channel][:, row*cols+col]
     normalize = lambda pixel: pixel/255
@@ -8,9 +10,9 @@ def generate(model, latent=None, batch=1, deterministic=False):
     pixels = np.zeros(shape=(batch,) + (shape if isinstance(shape, tuple) else shape[0])[1:])
     latent_vec = None if latent is None else np.expand_dims(np.ones(batch).astype(int)*latent, -1)
     batch, rows, cols, channels = pixels.shape
-    for row in range(rows):
-        for col in range(cols):
-            for channel in range(channels):
+    for row in tqdm(range(rows)):
+        for col in tqdm(range(cols)):
+            for channel in tqdm(range(channels)):
                 ps = get_color(model, pixels, cols, row, col, channel, latent_vec)
                 if deterministic:
                     pixels[:, row, col, channel] = normalize(np.array([np.argmax(p) for p in ps]))
@@ -44,8 +46,10 @@ def clean_data(data, rgb=True, latent=True):
 
     return data, labels
 
-def sparse_targets(generator):
+def sparse_labels(labels):
+    return np.expand_dims(np.argmax(labels, -1), -1)
+
+def sparse_labels_generator(generator):
     while True:
         inputs, labels = next(generator)
-        labels = np.expand_dims(np.argmax(labels, -1), -1)
-        yield (inputs, labels)
+        yield (inputs, sparse_labels(labels))
