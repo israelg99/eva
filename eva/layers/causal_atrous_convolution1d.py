ec8ed7dffee8e3e15ce5db0b86f249d29af578b4
Israel Gilyadov <israelg99@gmail.com>
2017-02-21 11:11:35 +0200

    Engineer causal atrous conv1d

---
+++ eva/layers/causal_atrous_convolution1d.py
@@ -0,0 +1,21 @@
+import keras.backend as K
+from keras.layers import AtrousConvolution1D
+from keras.utils.np_utils import conv_output_length
+
+class CausalAtrousConvolution1D(AtrousConvolution1D):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.border_mode != 'valid':
+            raise ValueError("Causal mode dictates border_mode=valid.")
+
+    def get_output_shape_for(self, input_shape):
+        length = conv_output_length(input_shape[1] + self.atrous_rate * (self.filter_length - 1),
+                                    self.filter_length,
+                                    self.border_mode,
+                                    self.subsample[0],
+                                    dilation=self.atrous_rate)
+
+        return (input_shape[0], length, self.nb_filter)
+
+    def call(self, x, mask=None):
+        return super().call(K.asymmetric_temporal_padding(x, self.atrous_rate * (self.filter_length - 1), 0), mask)
