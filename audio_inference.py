Update inference routine and fix units counting

audio_inference.py
@@ -27,8 +27,8 @@ FILTERS = 32
 DEPTH = 8
 STACKS = 4
 BINS = 256
-SAMPLE = 4000
-LENGTH = 1 + compute_receptive_field(SAMPLE, DEPTH, STACKS)[0]
+RATE = 4000
+LENGTH = RATE*2 + compute_receptive_field(RATE, DEPTH, STACKS)[0]
 
 #%% Model.
 print('Loading the model.')
@@ -52,7 +52,7 @@ def save():
     pcm8_64bit_wide = to_pcm8(audio)
 
     for i in tqdm(range(audio.shape[0])):
-        scipy.io.wavfile.write('audio{}.wav'.format(i), SAMPLE, pcm8_64bit_wide[i])
+        scipy.io.wavfile.write('audio{}.wav'.format(i), RATE, pcm8_64bit_wide[i])
 
 def save_gracefully(signal, frame):
     save()
@@ -71,7 +71,7 @@ for i in tqdm(range(UNITS+LENGTH-1)):
     samples[:, x] = M.predict(samples[:, i:i+LENGTH], batch_size=1)[0]
     samples[:,x,np.argmax(samples[:,x], axis=-1)] += 1-np.sum(samples[:, x], axis=-1)
     audio[:, x] = np.array([np.random.choice(256, p=p) for p in samples[:, x]])
-    if i>0 and ((i-LENGTH+1) % (SAMPLE//2) == 0):
-        print(str((i-LENGTH+1)/(SAMPLE//2)) + " Seconds generated!")
+    if i-LENGTH+1>0 and ((i-LENGTH+1) % (RATE//2) == 0):
+        print(str((i-LENGTH+1)/(RATE//2)) + " Seconds generated!")
 
 save()