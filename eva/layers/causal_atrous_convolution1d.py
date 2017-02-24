2fcca753e2877d21e158d5e4a3923e1d22e61608
Israel Gilyadov <israelg99@gmail.com>
2017-02-24 03:02:38 +0200

    Add mask A to causal atrous conv1d (WIP)

--- eva/layers/causal_atrous_convolution1d.py
+++ eva/layers/causal_atrous_convolution1d.py
@@ -3,11 +3,15 @@ from keras.layers import AtrousConvolution1D
 from keras.utils.np_utils import conv_output_length
 
 class CausalAtrousConvolution1D(AtrousConvolution1D):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, mask_type='B', **kwargs):
         super().__init__(*args, **kwargs)
         if self.border_mode != 'valid':
             raise ValueError("Causal mode dictates border_mode=valid.")
         self.length = self.atrous_rate * (self.filter_length - 1)
+
+        # XXX debug.
+        if mask_type == 'A':
+            self.length = 3
 
     def get_output_shape_for(self, input_shape):
         length = conv_output_length(input_shape[1] + self.length,
