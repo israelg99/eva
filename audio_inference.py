Fix units counting

audio_inference.py
@@ -65,13 +65,13 @@ BATCH_SIZE = 1
 samples = np.zeros(shape=(BATCH_SIZE, UNITS, BINS))
 audio = np.zeros(shape=(BATCH_SIZE, UNITS))
 for i in tqdm(range(UNITS+LENGTH-1)):
-    if i >= UNITS:
+    if i >= UNITS-1:
         break
     x = i+LENGTH-1
     samples[:, x] = M.predict(samples[:, i:i+LENGTH], batch_size=1)[0]
     samples[:,x,np.argmax(samples[:,x], axis=-1)] += 1-np.sum(samples[:, x], axis=-1)
     audio[:, x] = np.array([np.random.choice(256, p=p) for p in samples[:, x]])
-    if (i-LENGTH+1) % (SAMPLE//2) == 0:
-        print(str((i-LENGTH+1)//(SAMPLE//2)) + " Seconds generated!")
+    if i>0 and ((i-LENGTH+1) % (SAMPLE//2) == 0):
+        print(str((i-LENGTH+1)/(SAMPLE//2)) + " Seconds generated!")
 
 save()